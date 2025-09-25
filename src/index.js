5473b48729a7dec4ad08d253d93ecb736e07470a
Rik Brown <rik@rik.codes>
2025-09-25 15:22:58 -0400

    <a> link URLs

--- src/index.js
+++ src/index.js
@@ -44,11 +44,11 @@ const run = async () => {
 						</tr>
 						<tr>
 							<td><strong>${ error ? '🔴' : previewUrl ? '✅' : '🟨' } Preview:</strong></td>
-							<td>${ error ? '*Error*' : (previewUrl || '*Pending*') }</td>
+							<td>${ error ? '*Error*' : (previewUrl ? `<a target='_blank' href='${ previewUrl }'>${ previewUrl }</a>` : '*Pending*') }</td>
 						</tr>
 						<tr>
 							<td><strong>🔍 Inspect:</strong></td>
-							<td>${ error ? '*Error*' : (inspectUrl || '*Pending*') }</td>
+							<td>${ error ? '*Error*' : (inspectUrl ? `<a target='_blank' href='${ inspectUrl }'>${ inspectUrl }</a>` : '*Pending*') }</td>
 						</tr>
 					</table>
 
